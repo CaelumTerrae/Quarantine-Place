3b919279a47f182024ae533a5ed6673c1a1fbef0
CaelumTerrae <gavinlifrieri@gmail.com>
2020-03-20 16:29:03 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+# ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# homepage route
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+# run
+if __name__ == '__main__':
+    app.run()
